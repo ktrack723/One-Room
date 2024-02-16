fefd7f918f4099587b0c42ec73d58dd4fb2b1328
SeungGeon Kim <keithrek@hanmail.net>
2024-02-17 03:34:49 +0900

    WIP

---
+++ Assets/Scripts/csBGMRandomizer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csBGMRandomizer : MonoBehaviour
+{
+    [Header("Fetch on start")]
+
+    [SerializeField] private AudioSource audioSource;
+
+    [Header("Parameters")]
+
+    [SerializeField] private List<AudioClip> BGMs;
+
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = BGMs[Random.Range(0, BGMs.Count)];
+    }
+
+    void Update()
+    {
+        
+    }
+}
